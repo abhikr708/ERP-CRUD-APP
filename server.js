b9ccead9fe9ebf070b1efaff648e2ef7ed2998d4
Abhishek Kumar <abhikr708@gmail.com>
2024-11-08 12:27:19 +0530

    added logRequest middleware

--- server.js
+++ server.js
@@ -3,15 +3,27 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 const dbConnect = require('./config/db');
+const possport = require('passport');
+const LocalStrategy =  require('passport-local').Strategy;
 
 // Load the config from env
 require('dotenv').config();
 const PORT = process.env.PORT || 4000;
 
+// Middleware Function
+const logRequest = (req, res, next)=>{
+    console.log(`${new Date().toLocaleString()}, Request Made to: ${req.originalUrl}`);
+    next(); // move to the next phase
+}
+
 // Establish connection to the database
 dbConnect();
 
-// __________Import the Routes__________
+// _______Routes_______
+app.use(logRequest);
+app.get('/', (req, res)=>{
+    res.send("WELCOME....");
+})
 
 // Sales Manager Routes
 const salesManagerRoutes = require('./routes/salesManagerRoutes');
